b97a0fcf0768efa79b05c5c452afaaf2354b83ae
Matt Machell <matt.machell@digital.justice.gov.uk>
2018-11-16 21:40:57 +0000

    Updated list of attendees

--- mapdata.js
+++ mapdata.js
@@ -1,65 +1,157 @@
-var teams = 
-[
-    {
-        "name": "Office of the Public Guardian Digital Team",
-        "latlong": [
-            52.475,
-            -1.906
-        ]
-    },
-    {
-        "name": "Crown Commercial Services",
-        "latlong": [
-            51.566,
-            -3.027
-        ]
-    },
-    {
-        "name":"HPPPS Digital Studio",
-        "latlong":[
-            53.381, -1.470
-        ]
-    },
-    {
-        "name":"Ofsted",
-        "latlong":[52.922, -1.474]
-    },
-    {
-        "name":"DVSA",
-        "latlong":[52.954, -1.158]
-    },
-    {
-        "name":"Kainos",
-        "latlong":[52.478, -1.906]
-    },
-    {
-        "name":"HM Land Registry",
-        "latlong":[52.948,-1.151]
-    },
-    {
-        "name":"Leicestershire County Council",
-        "latlong":[ 52.655,-1.188]
-    },
-    {
-        "name":"HM Land Registry",
-        "latlong":[52.6784, -2.4453]
-    },
-    {
-        "name":"HMRC",
-        "latlong":[52.6784, -2.4453]
-    },
-    {
-        "name":"Cap Gemini",
-        "latlong":[52.676,-2.442]
-    },
-    {
-        "name":"University of Wolverhampton",
-        "latlong":[52.587,-2.128]
-    }, 
-    //,
-   /*  
-    {
-        "name":"",
-        "latlong":[]
-    }, */
-]
+var teams =
+    [
+        {
+            "name": "Office of the Public Guardian Digital Team",
+            "latlong": [
+                52.475,
+                -1.906
+            ]
+        },
+        {
+            "name": "Crown Commercial Services",
+            "latlong": [
+                51.566,
+                -3.027
+            ]
+        },
+        {
+            "name": "HPPPS Digital Studio",
+            "latlong": [
+                53.381, -1.470
+            ]
+        },
+        {
+            "name": "Ofsted",
+            "latlong": [52.922, -1.474]
+        },
+        {
+            "name": "DVSA",
+            "latlong": [52.954, -1.158]
+        },
+        {
+            "name": "Kainos",
+            "latlong": [52.478, -1.906]
+        },
+        {
+            "name": "HM Land Registry",
+            "latlong": [52.948, -1.151]
+        },
+        {
+            "name": "Leicestershire County Council",
+            "latlong": [52.655, -1.188]
+        },
+        {
+            "name": "HM Land Registry",
+            "latlong": [52.678, -2.445]
+        },
+        {
+            "name": "HMRC",
+            "latlong": [52.678, -2.445]
+        },
+        {
+            "name": "Cap Gemini",
+            "latlong": [52.676, -2.442]
+        },
+        {
+            "name": "University of Wolverhampton",
+            "latlong": [52.587, -2.128]
+        },
+        {
+            "name": "West Midlands Police",
+            "latlong": [52.587, 2.128]
+        },
+        {
+            "name": "Northamptonshire County Council",
+            "latlong": [52.235, -0.896]
+        },
+        {
+            "name": "HS2",
+            "latlong": [52.483727, -1.89842]
+        },
+        {
+            "name": "West Midlands Growth Company",
+            "latlong": [52.480, -1.907]
+        },
+        {
+            "name": "Birmingham and Solihull Mental Health Trust",
+            "latlong": [52.484, -1.917]
+        },
+        {
+            "name": "West Midlands Police",
+            "latlong": [52.516, -1.998]
+        },
+        {
+            "name": "West Midlands Police",
+            "latlong": [52.511, -2.082]
+        },
+        {
+            "name": "Hays",
+            "latlong": [52.482, -1.896]
+        },
+        {
+            "name":"Fusion Meetup",
+            "latlong":[52.479, -1.898]
+        },
+        {
+            "name":"Big Lottery Fund",
+            "latlong":[52.472, -1.918]
+        },
+        {
+            "name":"University of Birmingham",
+            "latlong":[52.451, -1.930]
+        },
+        {
+            "name":"West Midlands Combined Authority",
+            "latlong":[52.487, -1.899]
+        },
+        {
+            "name":"West Midlands Fire Service",
+            "latlong":[52.485, -1.876]
+        },
+        {
+            "name":"Warwickshire County Council",
+            "latlong":[52.282, -1.589]
+        },
+        {
+            "name":"Curium Solutions",
+            "latlong":[52.481, -1.900]
+        },
+        {
+            "name":"South Worcestershire Councils",
+            "latlong":[52.114, -2.078]
+        },
+        {
+            "name":"Coventry &amp; Warwickshire Local Enterprise Partnership",
+            "latlong":[52.282, -1.591]
+        },
+        {
+            "name":"QA Learning",
+            "latlong":[52.476, -1.898]
+        },
+        {
+            "name":"Education &amp; Skills Funding Agency",
+            "latlong":[52.400, -1.508]
+        },
+        {
+            "name":"Department of Work and Pensions",
+            "latlong":[52.370,1.265]
+        },
+        {
+            "name":"RnR Organisation",
+            "latlong":[52.477, -1.886]
+        },
+        {
+            "name":"Crown Commercial Service",
+            "latlong":[52.629, 1.365]
+        },
+        {
+            "name":"CCS",
+            "latlong":[ 51.514,-0.119]
+        },
+        //
+        /*  
+         {
+             "name":"",
+             "latlong":[]
+         }, */
+    ]
